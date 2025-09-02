0453cf1311b504c5b0feb54e3227afc40371f08f
Ranit Deria <121992255+RanitDERIA@users.noreply.github.com>
2025-09-02 18:17:35 +0530

    Update config.js

--- dashboard/src/config.js
+++ dashboard/src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const config = {
-  backendUrl: process.env.REACT_APP_BACKEND_URL || "https://elevate-broking.onrender.com",
+  backendUrl: "https://elevate-broking.onrender.com",
 };
 
 export default config;
