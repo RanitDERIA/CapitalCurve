f601ef2267209c3180672c314a7c0247c9fbe5ba
Ranit Deria <121992255+RanitDERIA@users.noreply.github.com>
2025-09-02 19:51:45 +0530

    Update Home.js

--- dashboard/src/components/Home.js
+++ dashboard/src/components/Home.js
@@ -1,15 +1,13 @@
-// src/components/Home.jsx
 import React from "react";
+
+import Dashboard from "./Dashboard";
 import TopBar from "./TopBar";
 
 const Home = () => {
   return (
     <>
       <TopBar />
-      <div className="home-container">
-        <h2>Welcome to the Trading Dashboard</h2>
-        <p>Select "Dashboard" from the menu to view your portfolio, orders, holdings, and more.</p>
-      </div>
+      <Dashboard />
     </>
   );
 };
