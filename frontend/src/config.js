d8e2c16daa831a3c3d608ed2a1c97cbe5c12504b
Ranit Deria <121992255+RanitDERIA@users.noreply.github.com>
2025-09-02 15:48:38 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,5 @@
 // frontend/src/config.js
 const API_BASE_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:3002";
-export default API_BASE_URL;
+const DASHBOARD_URL = process.env.REACT_APP_DASHBOARD_URL || "https://elevate-dashboard-l8sx.onrender.com";
+
+export { API_BASE_URL, DASHBOARD_URL };
