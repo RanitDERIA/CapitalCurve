cb205427625260be7d96699b18b2ad50405ca98a
Ranit Deria <121992255+RanitDERIA@users.noreply.github.com>
2025-09-02 19:36:54 +0530

    Update Dashboard.js

--- dashboard/src/components/Dashboard.js
+++ dashboard/src/components/Dashboard.js
@@ -1,4 +1,4 @@
-// src/components/Dashboard.jsx
+// dashboard/src/components/Dashboard.jsx
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
@@ -9,6 +9,7 @@ import Orders from "./Orders";
 import Positions from "./Positions";
 import Summary from "./Summary";
 import WatchList from "./WatchList";
+
 import { GeneralContextProvider } from "./GeneralContext";
 
 const Dashboard = () => {
